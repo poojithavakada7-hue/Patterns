fcaeb409d82c5f6fa8eb0f52b88fd9dbe0e03d1c
poojithavakada7-hue <poojithavakada7@gmail.com>
2026-07-22 18:24:33 +0530

    Add files via upload

---
+++ SquarePattern.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class SquarePattern{
+public static void main(String Args[]){
+Scanner Sc=new Scanner(System.in);
+System.out.println("enter n:");
+int n=Sc.nextInt();
+for(int i =0;i<=n;i++)
+{
+for(int j=0;j<=n;j++){
+System.out.print("* ");
+}
+System.out.println("");
+
+}
+}
+}
