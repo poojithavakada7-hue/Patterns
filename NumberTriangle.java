0f412161917e6b55f7c2d6a1074b96b66c84b48c
poojithavakada7-hue <poojithavakada7@gmail.com>
2026-07-26 18:26:33 +0530

    Add files via upload

---
+++ NumberTriangle.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class NumberTriangle{
+public static void main(String [] args){
+	Scanner sc=new Scanner(System.in);
+	System.out.println("Enter n value:");
+	int n=sc.nextInt();
+	int num = 1;
+
+for(int i = 1; i <= n; i++) {
+    for(int j = 1; j <= i; j++) {
+        System.out.print(num + " ");
+        num++;
+    }
+    System.out.println();
+}
+}
+}
