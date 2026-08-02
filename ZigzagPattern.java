6000555fef52e9644b753af41475f1069a43b398
Vakada Poojitha <poojithavakada7@gmail.com>
2026-08-02 19:19:35 +0530

    Add files via upload

---
+++ ZigzagPattern.java
@@ -0,0 +1,18 @@
+public class ZigzagPattern {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 3; i++) {
+
+            for (int j = 1; j <= 15; j++) {
+
+                if ((i + j) % 4 == 0 || (i == 2 && j % 4 == 0))
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+
+            }
+
+            System.out.println();
+        }
+    }
+}
